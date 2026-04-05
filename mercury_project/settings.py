2d28616044032f9422721f6dc532785b92b5e95d
pepan222 <ferczu72@gmail.com>
2026-04-05 17:37:05 +0300

    SEO: add sitemap.xml and enable django.contrib.sitemaps

--- mercury_project/settings.py
+++ mercury_project/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
     'crispy_forms',
     'crispy_bootstrap5',
     'whitenoise.runserver_nostatic',
--- mercury_project/urls.py
+++ mercury_project/urls.py
@@ -2,7 +2,59 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.urls import re_path 
+from django.urls import re_path
+from django.contrib.sitemaps import views as sitemap_views
+from django.urls import reverse
+
+# Импорт моделей для sitemap
+from menu.models import Dish, Category
+
+
+class StaticViewSitemap:
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        # Список URL-имён для статических страниц
+        return [
+            'core:home',
+            'menu:menu_list',
+            'core:gallery',
+            'core:contacts',
+            'bookings:booking',
+            'orders:cart'
+        ]
+
+    def location(self, item):
+        return reverse(item)
+
+
+class DishSitemap:
+    changefreq = "weekly"
+    priority = 0.9
+
+    def items(self):
+        return Dish.objects.filter(is_available=True)
+
+    def lastmod(self, obj):
+        # Если у модели Dish есть поле updated_at, иначе вернёт None
+        return getattr(obj, 'updated_at', None)
+
+
+class CategorySitemap:
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return Category.objects.all()
+
+
+sitemaps = {
+    'static': StaticViewSitemap,
+    'dishes': DishSitemap,
+    'categories': CategorySitemap,
+}
+
 
 urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
@@ -14,6 +66,9 @@ urlpatterns = [
     path('bookings/', include('bookings.urls')),
     path('reviews/', include('reviews.urls')),
     path('accounts/', include('users.urls')),
+    # Карта сайта
+    path('sitemap.xml', sitemap_views.sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 urlpatterns += [
